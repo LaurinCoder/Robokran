21ee6d8e7047a58a5ba5ec27d9dc7370da15760b
LaurinCoder <anmeldung1@gmx.at>
2018-02-06 12:41:19 +0100

    add Boku Keller IP

--- mainwindow.h
+++ mainwindow.h
@@ -126,7 +126,8 @@ private:
     unsigned int vRatio     = 0;
 
  /* AB HIER FÜR OPC_UA */
-    #define SpsIp "opc.tcp://169.254.25.28:4840"   //IP Adresse Labor Tests mit AutomationPC
+    #define SpsIp "opc.tcp://192.168.0.1:4840"   //IP Adresse Boku Keller
+    //#define SpsIp "opc.tcp://169.254.25.28:4840"   //IP Adresse Labor Tests mit AutomationPC
     //#define SpsIp "opc.tcp://169.254.123.90:4840"   //IP Adresse Lasco Vor-Ort
     QTimer *activeTimer;  //für OPC_UA
     UA_Client *client;
